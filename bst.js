79cc849f7f0b151b92ce239983eb3d22b69cadf7
SirIsaacNeutron <dbadir@cox.net>
2023-03-22 22:36:39 -0700

    Add BST insertion and deletion

--- bst.js
+++ bst.js
@@ -1,7 +1,74 @@
 export default class BST {
-    constructor(array) {
-        this.root = buildTree(array, 0, array.length - 1)
+    constructor(array = null) {
+        if (array === null) {
+            this.root = null
+        }
+        else {
+            this.root = buildTree(array, 0, array.length - 1)
+        }
     }
+
+    insert = value => {
+        this.root = insertRecursive(this.root, value)
+    }
+
+    delete = value => {
+        this.root = deleteRecursive(this.root, value)
+    }
+}
+
+// Doesn't insert duplicates!
+function insertRecursive(root, value) {
+    if (root === null) {
+        root = new Node(value)
+        return root
+    }
+    if (value < root.data) {
+        root.left = insertRecursive(root.left, value)
+    }
+    else if (value > root.data) {
+        root.right = insertRecursive(root.right, value)
+    }
+
+    return root
+}
+
+function deleteRecursive(root, value) {
+    if (root === null) {
+        return root
+    }
+
+    if (value < root.data) {
+        root.left = deleteRecursive(root.left, value)
+    }
+    else if (value > root.data) {
+        root.right = deleteRecursive(root.right, value)
+    }
+    else {
+        // If node has only one child
+        if (root.left === null) {
+            return root.right
+        }
+        else if (root.right === null) {
+            return root.left
+        }
+
+        // If node has two children, then we need the smallest value in the right
+        // subtree
+        root.data = minTreeValue(root.right)
+        root.right = deleteRecursive(root.right, root.data)
+    }
+    return root
+}
+
+function minTreeValue(root) {
+    let minValue = root.data
+    // Elements less than the root are always to its left
+    while (root.left !== null) {
+        minValue = root.left.data
+        root = root.left
+    }
+    return minValue
 }
 
 function buildTree(array, start, end) {
